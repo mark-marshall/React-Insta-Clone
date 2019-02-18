7d871f1a284539ea4c501ed42207f4c4431e2f0e
mark-marshall <mark.marshallgp@gmail.com>
2019-02-18 13:50:53 +0000

    [MVP][DayOne] Added all assets for search bar

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default function SearchBar () {
     return (
         <>
-        <h1>Search Bar Goes Here</h1>
+        <nav>
+            <a><img src={require('./SearchBarAssets/logo.png')}/></a>
+            <a><img src={require('./SearchBarAssets/word-logo.png')}/></a>
+            <input placeholder="Search"/>
+            <a><img src={require('./SearchBarAssets/compass.png')}/></a>
+            <a><img src={require('./SearchBarAssets/heart.png')}/></a>
+            <a><img src={require('./SearchBarAssets/user.png')}/></a>
+        </nav>
         </>
     )
 }
