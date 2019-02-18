b2955c437f94b1dfe0b0fb3918800df497b4569f
mark-marshall <mark.marshallgp@gmail.com>
2019-02-18 15:34:02 +0000

    [MVP][DayOne] Reformated all code

--- instagram/src/components/CommentSection/CommentSection.jsx
+++ instagram/src/components/CommentSection/CommentSection.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './CommentSection.css';
 import PT from 'prop-types';
 
-export default function CommentSection ({ comments }) {
-    return (
-        <div className="published-comments">
-           {
-               comments.map(comment => <div className="comments" key={comment.text}><span className="commenter">{comment.username}</span> {comment.text}</div>)
-           }
+export default function CommentSection({ comments }) {
+  return (
+    <div className="published-comments">
+      {comments.map(comment => (
+        <div className="comments" key={comment.text}>
+          <span className="commenter">{comment.username}</span> {comment.text}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 CommentSection.propTypes = {
-    comments: PT.arrayOf(PT.object.isRequired).isRequired,
-}
+  comments: PT.arrayOf(PT.object.isRequired).isRequired
+};
--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -32,7 +32,7 @@ export default function PostContainer({
         <div className="likes">{likes} likes</div>
         <CommentSection comments={comments} />
         <span className="time-stamp">{timestamp}</span>
-        <div className="add-comment-divider"></div>
+        <div className="add-comment-divider" />
         <input className="add-comment" placeholder="Add a comment..." />
       </div>
     </div>
@@ -40,11 +40,10 @@ export default function PostContainer({
 }
 
 PostContainer.propTypes = {
-    thumbnail: PT.string.isRequired,
-    image: PT.string.isRequired,
-    username: PT.string.isRequired,
-    likes: PT.number.isRequired,
-    comments: PT.arrayOf(PT.object.isRequired).isRequired,
-    timestamp: PT.string.isRequired,
-}
-
+  thumbnail: PT.string.isRequired,
+  image: PT.string.isRequired,
+  username: PT.string.isRequired,
+  likes: PT.number.isRequired,
+  comments: PT.arrayOf(PT.object.isRequired).isRequired,
+  timestamp: PT.string.isRequired
+};
