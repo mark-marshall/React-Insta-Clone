ef68668ae52f7947beb9462b3bdeb5340a293ec6
mark-marshall <mark.marshallgp@gmail.com>
2019-02-18 14:54:22 +0000

    [MVP][DayOne] Added PostContainer styles

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
+import './PostContainer.css';
 
 export default function PostContainer({ thumbnail, username, image, likes, comments, timestamp }) {
   return (
     <div className="post-container">
-      <header>
-          <img alt="company logo" src={thumbnail}/>
-          {username}
-      </header>
-      <img alt="post" src={image}/>
-      <div>{likes} likes</div>
+      <div className="post-header-section">
+          <img className="thumbnail-image" alt="company logo" src={thumbnail}/>
+          <div className="username">{username}</div>
+      </div>
+      <img className="main-image" alt="post" src={image}/>
+      <div className="under-image-container">
+      <img className="post-reaction-image" alt="heart" src={require('./PostContainerAssets/heart.png')}/>
+      <img className="post-reaction-image"  alty="comment" src={require('./PostContainerAssets/comment.png')}/>
+      <div className="likes">{likes} likes</div>
     <CommentSection comments={comments}/>
     <span className="time-stamp">{timestamp}</span>
+    </div>
     </div>
   );
 }
