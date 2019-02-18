86476b6faa2afc722a2cbef7dc23553f9099be6d
mark-marshall <mark.marshallgp@gmail.com>
2019-02-18 14:21:06 +0000

    [MVP][DayOne] Added SearchBar styles

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
+import './SearchBar.css';
 
-export default function SearchBar () {
-    return (
-        <>
-        <nav>
-            <a><img src={require('./SearchBarAssets/logo.png')}/></a>
-            <a><img src={require('./SearchBarAssets/word-logo.png')}/></a>
-            <input placeholder="Search"/>
-            <a><img src={require('./SearchBarAssets/compass.png')}/></a>
-            <a><img src={require('./SearchBarAssets/heart.png')}/></a>
-            <a><img src={require('./SearchBarAssets/user.png')}/></a>
-        </nav>
-        </>
-    )
+export default function SearchBar() {
+  return (
+    <nav>
+      <div className="search-bar-left">
+        <a>
+          <img
+            alt="logo"
+            className="nav-item small-icon"
+            src={require('./SearchBarAssets/logo.png')}
+          />
+        </a>
+        <a>
+          <img
+            alt="instagram writing"
+            className="nav-item logo-writing"
+            src={require('./SearchBarAssets/word-logo.png')}
+          />
+        </a>
+      </div>
+      <div className="search-bar-middle">
+        <input className="input" placeholder="Search" />
+      </div>
+      <div className="search-bar-right">
+        <a>
+          <img
+            alt="compass"
+            className="nav-item small-icon"
+            src={require('./SearchBarAssets/compass.png')}
+          />
+        </a>
+        <a>
+          <img
+            alt="heart"
+            className="nav-item small-icon"
+            src={require('./SearchBarAssets/heart.png')}
+          />
+        </a>
+        <a>
+          <img
+            alt="user"
+            className="nav-item small-icon"
+            src={require('./SearchBarAssets/user.png')}
+          />
+        </a>
+      </div>
+    </nav>
+  );
 }
