981fe5dacb9897f20b15bb2f1211c1b941b0310f
mark-marshall <mark.marshallgp@gmail.com>
2019-02-19 12:46:16 +0000

    [MVP][DayTwo] Converted CommentSection into a class component with comments on state

--- instagram/src/components/CommentSection/CommentSection.jsx
+++ instagram/src/components/CommentSection/CommentSection.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CommentSection.css';
 import './AddComment';
 import PT from 'prop-types';
 import AddComment from './AddComment';
 
-export default function CommentSection({ comments }) {
-  return (
-    <div className="comment-section">
+class CommentSection extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      comments: props.comments,
+    }
+  }
+  render (){
+    return (
+      <div className="comment-section">
       <div className="published-comments">
-        {comments.map(comment => (
+        {this.state.comments.map(comment => (
           <div className="comments" key={comment.text}>
             <span className="commenter">{comment.username}</span> {comment.text}
           </div>
@@ -16,9 +23,13 @@ export default function CommentSection({ comments }) {
       </div>
       <AddComment />
     </div>
-  );
+    )
+  }
 }
 
 CommentSection.propTypes = {
   comments: PT.arrayOf(PT.object.isRequired).isRequired
 };
+
+
+export default CommentSection;
