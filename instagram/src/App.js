92b588bc6f1f8d926c18dbf6bbfb796babf4ffe7
mark-marshall <mark.marshallgp@gmail.com>
2019-02-20 13:00:17 +0000

    [MVP][DayThree] Moved App.JS code into PostsPage.jsx

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,75 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      post: [],
-      searchBox: ''
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      post: dummyData
-    });
-  }
-
-  currentSearch = event => {
-    event.preventDefault();
-    this.setState({
-      searchBox: event.target.value
-    });
-    this.clearSearch();
-  };
-
-  filterSearches = event => {
-    event.preventDefault();
-    if (this.state.searchBox) {
-      this.setState(prevState => ({
-        post: prevState.post.filter(
-          item => item.username === this.state.searchBox
-        )
-      }));
-    } else {
-      this.setState({
-        post: dummyData
-      });
-    }
-  };
-
-  clearSearch = () => {
-    this.setState({
-      currentSearch: '',
-      post: dummyData
-    });
-  };
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <SearchBar
-            searchBox={this.state.searchBox}
-            currentSearch={this.currentSearch}
-            filterSearches={this.filterSearches}
-          />
-        </header>
-        {this.state.post.map(post => (
-          <PostContainer
-            key={post.imageUrl}
-            thumbnail={post.thumbnailUrl}
-            username={post.username}
-            image={post.imageUrl}
-            likes={post.likes}
-            comments={post.comments}
-            timestamp={post.timestamp}
-          />
-        ))}
+      <div>
+        <PostsPage />
       </div>
     );
   }
