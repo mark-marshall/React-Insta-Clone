7cacfed5f69129deaa7b0e9657f91faa2ab40ee3
mark-marshall <mark.marshallgp@gmail.com>
2019-02-20 13:58:18 +0000

    [MVP][DayThree] Added LoginPage class component with no styles

---
+++ instagram/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './LoginPage.css';
+
+class LoginPage extends React.Component {
+  state = {
+      username: '',
+      loggedIn: false,
+  };
+
+  userType = event => {
+      this.setState({
+          username: event.target.value,
+      })
+  }
+
+  login = () => {
+    localStorage.setItem('username', JSON.stringify(this.state.username));
+  };
+
+  componentDidMount() {
+      this.setState({
+        loggedIn: true,
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <input onChange={(event) => this.userType(event)} value={this.state.username} placeholder="Username" />
+        <input placeholder="Password" />
+        <button onClick={() => this.login()}>Log In</button>
+      </>
+    );
+  }
+}
